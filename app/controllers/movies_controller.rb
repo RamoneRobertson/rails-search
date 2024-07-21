9870a7dc88fb7e665fcf763fbb667b88cfddf093
Ramone Robertson <ramonerobertson10@gmail.com>
2024-07-21 11:51:54 +0900

    Implemented pgsearch gem to search for movies

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,2 +1,31 @@
 class MoviesController < ApplicationController
+  def index
+    if params[:query].present?
+      @movies = Movie.search_by_title_and_synopsis(params[:query])
+      # Search where query is IN title or synopsis
+      # sql_query = "title ILIKE :query OR synopsis ILIKE :query"
+      # @movies = Movie.where( sql_query, query: "%#{params[:query]}%")
+
+      # ASSOCIATION SEARCH
+      # sql_query = <<~SQL
+      #   movies.title ILIKE :query
+      #   OR movies.synopsis ILIKE :query
+      #   OR directors.first_name ILIKE :query
+      #   OR directors.last_name ILIKE :query
+      # SQL
+
+      # FULL TEXT SEARCH
+      #  sql_query = <<~SQL
+      #   movies.title @@ :query
+      #   OR movies.synopsis @@ :query
+      #   OR directors.first_name @@ :query
+      #   OR directors.last_name @@ :query
+      # SQL
+
+      # @movies = Movie.joins(:director).where(sql_query, query: "%#{params[:query]}%")
+
+    else
+      @movies = Movie.all
+    end
+  end
 end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,3 +1,19 @@
 class Movie < ApplicationRecord
   belongs_to :director
+
+  # Include the module PgSearch
+  include PgSearch::Model
+  # Name a method (search_by_title_and_synopsis)
+  pg_search_scope :search_by_title_and_synopsis,
+    # Tell pgsearch which columns to search through model director using their first and last anme
+    against: [ :title, :synopsis ],
+    associated_against: {
+      # Since there is an association between the director and movie we
+      director: [ :first_name, :last_name ]
+    },
+    using: {
+      # tsearch is the same as full text search
+      tsearch: { prefix: true } # This means you dont have to type the entire word for it to be true
+      # tesearch -> same as @@ in postgres
+    }
 end
