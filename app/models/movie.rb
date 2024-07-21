90a7d3d3927eafaceba3ba493f1cb4d6c254ecfd
Ramone Robertson <ramonerobertson10@gmail.com>
2024-07-21 12:16:20 +0900

    enabled multisearchable for pgsearch

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -3,17 +3,20 @@ class Movie < ApplicationRecord
 
   # Include the module PgSearch
   include PgSearch::Model
+  # This allows you to search for multiple models
+  multisearchable against: [:title, :synopsis]
+
   # Name a method (search_by_title_and_synopsis)
-  pg_search_scope :search_by_title_and_synopsis,
+  # pg_search_scope :search_by_title_and_synopsis,
     # Tell pgsearch which columns to search through model director using their first and last anme
-    against: [ :title, :synopsis ],
-    associated_against: {
+    # against: [ :title, :synopsis ],
+    # associated_against: {
       # Since there is an association between the director and movie we
-      director: [ :first_name, :last_name ]
-    },
-    using: {
+    #   director: [ :first_name, :last_name ]
+    # },
+    # using: {
       # tsearch is the same as full text search
-      tsearch: { prefix: true } # This means you dont have to type the entire word for it to be true
+      # tsearch: { prefix: true } # This means you dont have to type the entire word for it to be true
       # tesearch -> same as @@ in postgres
-    }
+    # }
 end
--- app/models/tv_show.rb
+++ app/models/tv_show.rb
@@ -1,2 +1,5 @@
 class TvShow < ApplicationRecord
+  # We can search both models movies and tv shows by using this
+  include PgSearch::Model
+  multisearchable against: [:title, :synopsis]
 end
